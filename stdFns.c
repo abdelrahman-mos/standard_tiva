0c7c706a2ed387bc7e9409944424a055fa0559c3
Abdelrahman Mostafa <boboelahlawy1234@hotmail.com>
2022-04-19 00:57:38 +0200

    editing

--- stdFns.c
+++ stdFns.c
@@ -14,7 +14,8 @@ void digitalEnable(Pin pin, PORT port) {
 				case PF3: mask = (1<<3); break;
 				case PF4: mask = (1<<4); break;
 			}
-			GPIO_PORTF_DIR_R |= mask;
+			GPIO_PORTF_DEN_R |= mask;
+			
 			break;
 	}
 }
@@ -22,24 +23,44 @@ void digitalEnable(Pin pin, PORT port) {
 void digitalWrite(Pin pin, digital I_O) {
 	switch (pin) {
 		case PF0:
+			GPIO_PORTF_DIR_R |= 1;
 			digitalEnable(PF0, PORTF);
-			GPIO_PORTF_DATA_R |= 1;
+			if (I_O)
+				GPIO_PORTF_DATA_R |= 1;
+			else
+				GPIO_PORTF_DATA_R &= ~1;
 			break;
 		case PF1:
+			GPIO_PORTF_DIR_R |= (1<<1);
 			digitalEnable(PF1, PORTF);
-			GPIO_PORTF_DATA_R |= (1<<1);
+			if (I_O)
+				GPIO_PORTF_DATA_R |= (1<<1);
+			else
+				GPIO_PORTF_DATA_R &= ~(1<<1);
 			break;
 		case PF2:
+			GPIO_PORTF_DIR_R |= (1<<2);
 			digitalEnable(PF2, PORTF);
-			GPIO_PORTF_DATA_R |= (1<<2);
+			if (I_O)
+				GPIO_PORTF_DATA_R |= (1<<2);
+			else
+				GPIO_PORTF_DATA_R &= ~(1<<2);
 			break;
 		case PF3:
+			GPIO_PORTF_DIR_R |= (1<<3);
 			digitalEnable(PF3, PORTF);
-			GPIO_PORTF_DATA_R |= (1<<3);
+			if (I_O)
+				GPIO_PORTF_DATA_R |= (1<<3);
+			else
+				GPIO_PORTF_DATA_R &= ~(1<<3);
 			break;
 		case PF4:
+			GPIO_PORTF_DIR_R |= (1<<4);
 			digitalEnable(PF4, PORTF);
-			GPIO_PORTF_DATA_R |= (1<<4);
+			if (I_O)
+				GPIO_PORTF_DATA_R |= (1<<4);
+			else
+				GPIO_PORTF_DATA_R &= ~(1<<4);
 			break;
 	}
 }
